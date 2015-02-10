fc94702b7fbddf699c59a145d7a3c354ff1e79cd
Christopher Aloi <ctaloi@gmail.com>
2015-02-10 15:24:01 -0500

    dropped grid, added timestamps

--- client/templates/gallery/gallery.js
+++ client/templates/gallery/gallery.js
@@ -1,12 +1,12 @@
 Template.gallery.helpers({
-  showPictures: function() {
+  getPictures: function() {
     return Pictures.find({}, {
       sort: {
         createdAt: -1
       }
     });
   },
-  showRecent: function(count) {
+  getRecentPictures: function(count) {
     return Pictures.find({}, {
       sort: {
         createdAt: -1
@@ -14,4 +14,51 @@ Template.gallery.helpers({
       limit: count
     });
   },
+  getPictureCount: function() {
+    return Pictures.find().count();
+  }
 });
+
+
+
+// http://meteorpad.com/pad/WEoyj82x6uC4XPZv5/Rows
+// Template.gallery.helpers({
+//   myArray: function () {
+//     var res = [];
+//     for (var i = 0; i < 17; i++) {
+//       res.push('Row '+i);
+//     }
+//     return res;
+//   },
+//
+//   arrayToRows: function (array) {
+//     var rows = [];
+//     var row = [];
+//     array.forEach(function (el, index) {
+//       if (index % 4 === 0) {
+//         rows.push(row);
+//         row = [];
+//       }
+//       row.push(el)
+//     });
+//     rows.splice(0,1);
+//     console.log(rows);
+//     return rows;
+//   }
+// });
+
+// * {
+//   -moz-box-sizing: border-box;
+//   box-sizing: border-box;
+// }
+//
+// body {
+//   font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+//   font-size: 16px;
+//   font-weight: normal;
+//   margin: 3em 0;
+//   padding: 0;
+// }
+//
+// .row:nth-child(odd) {
+//   background-color: #ccc;
